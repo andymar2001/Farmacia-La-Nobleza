00e3e9f53ce949dd2f253a764e9fa87fb351f3f4
Christopher Q <Christopher Q@LAPTOP-L025Q9E6>
2020-07-16 22:26:41 -0500

    error

--- Farmacia La Nobleza/src/database/MysqlDBConexion.java
+++ Farmacia La Nobleza/src/database/MysqlDBConexion.java
@@ -7,21 +7,50 @@ import java.sql.SQLException;
 public class MysqlDBConexion {
 	
 	public static Connection getConexion() {
+//		 String nombreBD="CovidFarma";
+//		  String usuario="root";
+//		 String contraseña="mysql";
+//		  String url="jdbc:mysql://localhost:3306/"+nombreBD+"?useUnicode=true&use"
+//				 +"JDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&"
+//				 +"serverTimezone=UTC";
+//		 
+//		 Connection cn = null;
+//		
+//		try {
+//			//obtener el drivers
+//			
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			//obtener coneccion	
+//			cn = DriverManager.getConnection(url,usuario,contraseña);
+//			
+//			if(cn!=null) {
+//				System.out.print("Conexion exitosa a la base de datos :");
+//			}
+//			
+//		} catch (ClassNotFoundException e) {
+//			System.out.print("Ocurre una classnotfound exception :" +e.getMessage());
+//		}catch (SQLException e) {
+//			System.out.print("ocurre una ecxepcion con sql eception :" +e.getMessage());
+//		}
+//		return cn;
+//		
+//		
+		
+		
+		
 		 String nombreBD="CovidFarma";
 		  String usuario="root";
 		 String contraseña="mysql";
 		  String url="jdbc:mysql://localhost:3306/"+nombreBD+"?useUnicode=true&use"
 				 +"JDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&"
 				 +"serverTimezone=UTC";
-		 
 		 Connection cn = null;
-		
 		try {
 			//obtener el drivers
-			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			//obtener coneccion	
-			cn = DriverManager.getConnection(url,usuario,contraseña);
+			Class.forName("com.mysql.jdbc.Driver");
+			String connectionURL="jdbc:mysql://localhost:3306/covidfarma?user=root&password=&useUnicode=true&characterEncoding";
+			//String connecctionURL="jdbc:mysql://localhost:3307/CovidFarma?user=root&password=mysql&useUnicode=true&characterEncoding=UTF-8";
+			cn=DriverManager.getConnection(connectionURL);
 			
 			if(cn!=null) {
 				System.out.print("Conexion exitosa a la base de datos :");
@@ -33,35 +62,6 @@ public class MysqlDBConexion {
 			System.out.print("ocurre una ecxepcion con sql eception :" +e.getMessage());
 		}
 		return cn;
-		
-		
-		
-		
-		/*
-		 String nombreBD="CovidFarma";
-		  String usuario="root";
-		 String contraseña="mysql";
-		  String url="jdbc:mysql://localhost:3306/"+nombreBD+"?useUnicode=true&use"
-				 +"JDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&"
-				 +"serverTimezone=UTC";
-		 Connection cn = null;
-		try {
-			//obtener el drivers
-			Class.forName("com.mysql.jdbc.Driver");
-			String connecctionURL="jdbc:mysql://localhost:3306/CovidFarma?user=root&password=&useUnicode=true&characterEncoding=UTF-8";
-			//String connecctionURL="jdbc:mysql://localhost:3307/CovidFarma?user=root&password=mysql&useUnicode=true&characterEncoding=UTF-8";
-			cn=DriverManager.getConnection(connecctionURL);
-			
-			if(cn!=null) {
-				System.out.print("Conexion exitosa a la base de datos :");
-			}
-			
-		} catch (ClassNotFoundException e) {
-			System.out.print("Ocurre una classnotfound exception :" +e.getMessage());
-		}catch (SQLException e) {
-			System.out.print("ocurre una ecxepcion con sql eception :" +e.getMessage());
-		}
-		return cn;*/
 	}
 
 }
